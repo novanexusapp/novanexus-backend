376123af3665d37e7bb1c345ad0b009a5a1fb84b
novanexusapp <arascetinkaya0233@gmail.com>
2026-08-15 15:10:12 +0300

    Update socket.js

--- api/socket.js
+++ api/socket.js
@@ -1,1 +1,22 @@
+import { Server } from "socket.io";
 
+export default function handler(req, res) {
+  if (!res.socket.server.io) {
+    const io = new Server(res.socket.server);
+
+    io.on("connection", (socket) => {
+      console.log("Bir kullanıcı bağlandı");
+
+      socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+      });
+
+      socket.on("chat file", (fileUrl) => {
+        io.emit("chat file", fileUrl);
+      });
+    });
+
+    res.socket.server.io = io;
+  }
+  res.end();
+}
